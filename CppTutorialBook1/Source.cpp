47fd5c26c01ceef3f29460791d0873358eeed694
Ömrüm Çetin <cetinomrum@gmail.com>
2021-11-09 18:28:34 +0300

    Some timer modifications

--- CppTutorialBook1/Source.cpp
+++ CppTutorialBook1/Source.cpp
@@ -4,6 +4,9 @@
 #include "AWeapon.h"
 #include <cassert>
 #include <vector>
+#include <algorithm>
+#include <ctime>
+#include <chrono>
 using namespace std;
 
 
@@ -14,13 +17,6 @@ void Show(vector<int> Arr)
         cout << i << " ";
     }
     cout << endl;
-}
-
-void Swap(int* elem1, int* elem2)
-{
-    int temp = *elem2;
-    *elem2 = *elem1;
-    *elem1 = temp;
 }
 
 void trysome(int* som)
@@ -34,15 +30,20 @@ void main()
 {
     vector<int> a{ 65 , 24 , 15 , 8 , 94 , 111, 256 , 56, 15, 99};
 
-    Show( a);
+    Show(a);
+    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+    sort(a.begin(), a.end());
+    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
 
+    clock_t end = clock();
+    cout << "Elapsed(ms) : " << (std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count()) / 1000000.0 << endl;
     for(size_t i = 0 ; i < a.size() ; i++)
     {
         cout << "Iteration " << i << endl;
         for(size_t j = 0; j < a.size() - i - 1 ; j++)
         {
             if(a[j] > a[j+1])
-                Swap(&a[j], &a[j+1]);
+                std::swap(a.at(j), a.at(j+1));
             Show(a);
         }
     }
